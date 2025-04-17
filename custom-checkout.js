ddc7357ac6d23352e4b1225f085e4369015f96c2
MontyPayDev <hadi.hassoun@montypay.com>
2025-04-17 15:50:06 +0300

    Added Buy It now V2 as comment

--- custom-checkout.js
+++ custom-checkout.js
@@ -96,3 +96,66 @@ async function BuyNowRedirect() {
     );
   }
 }
+
+///////////// Buy It Now V2
+
+// document.addEventListener(
+//   "click",
+//   async function (e) {
+//     const button = e.target.closest(
+//       ".shopify-payment-button__button.shopify-payment-button__button--unbranded"
+//     );
+//     if (button) {
+//       e.preventDefault();
+//       e.stopPropagation();
+//       // console.log("Dynamically loaded button clicked!");
+
+//       const shopifyCheckout = button.closest("form");
+//       if (shopifyCheckout) {
+//         // console.log("Form:", shopifyCheckout);
+
+//         // Get the required input with name="id"
+//         const inputId = shopifyCheckout.querySelector(
+//           'input[name="id"][required]'
+//         );
+
+//         if (inputId) {
+//           // console.log("Found required input with name='id':", inputId);
+//           // console.log("Value:", inputId.value);
+//           const quantityId = shopifyCheckout.querySelector(
+//             'input[name="quantity"]'
+//           );
+
+//           if (quantityId) {
+//             // console.log("Found input with name='quantity':", quantityId);
+//             // console.log("Value:", quantityId.value);
+
+//             fetch("/cart/add.js", {
+//               method: "POST",
+//               headers: {
+//                 "Content-Type": "application/json",
+//               },
+//               body: JSON.stringify({
+//                 id: inputId.value, // Replace with actual variant ID
+//                 quantity: quantityId.value,
+//               }),
+//             })
+//               .then((res) => res.json())
+//               .then((data) => {
+//                  window.location.href = "/pages/checkout";
+//               })
+//               .catch((err) => console.error("Cart add error:", err));
+//           } else {
+//             console.log("No input with name='quantity' found.");
+//           }
+//         } else {
+//           console.log("No required input with name='id' found.");
+//         }
+//       } else {
+//         console.log("No <form> element found.");
+//       }
+//       // await BuyNowRedirect();
+//     }
+//   },
+//   true // This ensures the event is captured before Shopify's event
+// );
